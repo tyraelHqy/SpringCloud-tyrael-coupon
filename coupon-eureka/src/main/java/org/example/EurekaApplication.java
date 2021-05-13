db619b7cbb53179fed73a96b8288997edf07e03d
tyraelHqy <752546894@qq.com>
2021-05-13 16:28:12 +0800

    初始化Eureka项目，配置成功

---
+++ coupon-eureka/src/main/java/org/example/EurekaApplication.java
@@ -0,0 +1,16 @@
+package org.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * Eureka启动入口
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class,args);
+    }
+}
