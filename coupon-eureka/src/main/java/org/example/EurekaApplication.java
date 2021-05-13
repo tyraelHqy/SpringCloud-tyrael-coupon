7740a36139300c989e349f935b6702bb7ca3d899
tyraelHqy <752546894@qq.com>
2021-05-13 16:31:07 +0800

    增加注释

--- coupon-eureka/src/main/java/org/example/EurekaApplication.java
+++ coupon-eureka/src/main/java/org/example/EurekaApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
- * Eureka启动入口
+ * Eureka启动入口，localhost:8000进行启动
  */
 @EnableEurekaServer
 @SpringBootApplication
