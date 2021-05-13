49aadd4f38072e08b63ff964a673bd0c1ee62869
tyraelHqy <752546894@qq.com>
2021-05-14 01:39:11 +0800

    1. 搭建Zuul网关模块

---
+++ coupon-gateway/src/main/java/org/example/ZuulGatewayApplication.java
@@ -0,0 +1,19 @@
+package org.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+/**
+ * 网关应用启动入口
+ * 1. @EnableZuulProxy 标识当前的应用时 Zuul Server
+ * 2. @SpringCloudApplication 组合了当前的 SpringBoot 应用 + 服务发现 + 熔断
+ */
+
+@EnableZuulProxy
+@SpringCloudApplication
+public class ZuulGatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ZuulGatewayApplication.class,args);
+    }
+}
